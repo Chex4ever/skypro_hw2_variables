250a11d1a4ffbcbb950fcaf4e8f760ee64e8c813
4ex4ever4ex@gmail.com <4ex4ever4ex@gmail.com>
2024-01-23 01:02:36 +0500

    Revert "ДЗ.2 - переменные"
    This reverts commit 0d760703267badd844e271ccb4445a49d8a5a4d1.

--- src/Main.java
+++ src/Main.java
@@ -1,98 +1,69 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Initial commit");
         //Задача 1
-        //Ну по-любому ведь имеется в виду инициализировать, а не объявить...
-        int sampleInt = 1234567890;
-        byte sampleByte = 123;
-        short sampleShort = 12345;
-        float sampleFloat = 0.1234567f;
-        double sampleDouble = 0.123456789101112f;
-        System.out.println("Значение переменной sampleInt с типом int равно " + sampleInt);
-        System.out.println("Значение переменной sampleByte с типом byte равно " + sampleByte);
-        System.out.println("Значение переменной sampleShort с типом short равно " + sampleShort);
-        System.out.println("Значение переменной sampleFloat с типом float равно " + sampleFloat);
-        System.out.println("Значение переменной sampleDouble с типом double равно " + sampleDouble);
-
+        //Объявите переменные типа int, byte, short, long, float, double.
+        //Выведите в консоль значение каждой переменной в формате «Значение переменной … с типом … равно …».
         //Задача 2
-        float a = 27.12f;
-        long b = 987_678_965_549L;
-        float c = 2.786f;
-        short d = 569;
-        short e = -159;
-        short f = 27897;
-        byte g = 67;
+        //Ниже дан список различных значений. Инициализируйте переменные, используйте изученные ранее типы переменных
+        //Значения:
+        //27.12
+        //987 678 965 549
+        //2,786
+        //569
+        //-159
+        //27897
+        //67
 
         //Задача 3
-        byte teacher1Students = 23;
-        byte teacher2Students = 27;
-        byte teacher3Students = 30;
-        short sheetsBought = 480;
-        int sheetsPerStudent = sheetsBought / (teacher1Students + teacher2Students + teacher3Students);
-        System.out.println("На каждого ученика рассчитано " + sheetsPerStudent + " листов бумаги");
+        //Три школьных учителя, Людмила Павловна, Анна Сергеевна и Екатерина Андреевна, ведут три класса.
+        //У Людмилы Павловны — 23 ученика , у Анны Сергеевны — 27 учеников, у Екатерины Андреевны — 30 учеников.
+        //Три учительницы закупили все вместе 480 листов бумаги на все три класса. Посчитайте, сколько достанется листов каждому ученику.
+        //Результат задачи выведите в консоль в формате: «На каждого ученика рассчитано … листов бумаги».
+        //Для объявления переменных не используйте тип var.
 
         //Задача 4
-        byte bottlesPer2Minute = 16;
-        int machinePowerPerMinute = bottlesPer2Minute / 2;
-        int bottlesPer20Minute = machinePowerPerMinute * 20;
-        int bottlesPerDay = machinePowerPerMinute * 60 * 24;
-        int bottlesPer3Days = bottlesPerDay * 3;
-        int bottlesPerMonth = bottlesPerDay * 30;
-        System.out.println("За 20 минут машина произвела " + bottlesPer20Minute + " штук бутылок");
-        System.out.println("За сутки машина произвела " + bottlesPerDay + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + bottlesPer3Days + " штук бутылок");
-        System.out.println("За месяц машина произвела " + bottlesPerMonth + " штук бутылок");
+        //Производительность машины для изготовления бутылок — 16 бутылок за 2 минуты. Какая производительность машины будет:
+        //за 20 минут,
+        //в сутки,
+        //за 3 дня,
+        //за 1 месяц?
+        //Рассчитывайте производительность работы машины в том случае, если она работает без перерыва заданный промежуток времени.
+        //Результаты подсчетов выведите в консоль в формате: «За … машина произвела … штук бутылок».
+        //Для объявления переменных не используйте тип var.
 
         //Задача 5
-        byte paintNeededOverall = 120;
-        byte paintPerRoomWhite = 2;
-        byte paintPerRoomBrown = 4;
-        int paintPerRoomOverall = paintPerRoomWhite + paintPerRoomBrown;
-        int rooms = paintNeededOverall / paintPerRoomOverall;
-        int paintNeededWhite = paintPerRoomWhite * rooms;
-        int paintNeededBrown = paintPerRoomBrown * rooms;
-        System.out.println("В школе, где " + rooms + " классов, нужно " + paintNeededWhite + " банок белой краски и "
-                + paintNeededBrown + " банок коричневой краски.");
+        //На ремонт школы нужно 120 банок краски двух цветов: белой и коричневой. На один класс уходит 2 банки белой и 4 банки коричневой краски. Сколько банок каждой краски было куплено?
+        //Выведите результат задачи в консоль в формате: «В школе, где … классов, нужно … банок белой краски и … банок коричневой краски».
+        //Для объявления переменных не используйте тип var.
 
         //Задача 6
-        byte bananaPieceWeightGr = 80;
-        byte milk100mlWeightGr = 105;
-        byte icePieceWeightGr = 100;
-        byte eggPieceWeightGr = 70;
-        int superBreakfastWeightGr = bananaPieceWeightGr * 5 + milk100mlWeightGr * 2 + icePieceWeightGr * 2
-                + eggPieceWeightGr * 4;
-        float superBreakfastWeightKg = superBreakfastWeightGr / 1000f;
-        System.out.println("Общий вес завтрака составляет " + superBreakfastWeightGr + " грамм, или "
-                + superBreakfastWeightKg + " килограмм");
+        //Спортсмены следят за своим весом и тщательно относятся к тому, что и сколько они съедают.
+        //Вот один из рецептов, по которому спортсмен готовит себе завтрак:
+        // Бананы — 5 штук (1 банан — 80 грамм).
+        // Молоко — 200 мл (100 мл = 105 грамм).
+        // Мороженое-пломбир — 2 брикета по 100 грамм.
+        // Яйца сырые – 4 яйца (1 яйцо — 70 грамм).
+        //Смешать всё в блендере — и готово.
+        //Подсчитайте вес (количество граммов) такого спортзавтрака, а затем переведите его в килограммы.
+        //Результат в граммах и килограммах напечатайте в консоль.
+        //Важное условие: если в рецепт нужно добавить несколько единиц какого-то продукта (с определенным весом), то нужно умножать количество единиц на вес в граммах. Например, если в рецепте указано, что нужно добавить 5 бананов по 80 грамм, то нужно количество бананов (5) умножить на вес одного (80 грамм), а не считать самим общую сумму граммов.
 
         //Задача 7
-        byte weightKgToLoose = 7;
-        int timeMinEffort = weightKgToLoose * 1000 / 250;
-        int timeMaxEffort = weightKgToLoose * 1000 / 500;
-        int timeAverage = (timeMaxEffort + timeMinEffort) / 2;
-        System.out.println("В лучшем случае спортсмен похудеет за " + timeMaxEffort + " дней; максимум за "
-                + timeMinEffort + " дней. В среднем потребуется " + timeAverage + " день");
+        //Правила соревнований обновились, и спортсмену, чтобы оставаться в своей весовой категории, нужно сбросить 7 кг. Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе от 250 до 500 грамм в день.
+        //Посчитайте, сколько дней уйдет на похудение, если спортсмен будет терять каждый день по 250 грамм, а сколько — если каждый день будет худеть на 500 грамм.
+        //Посчитайте, сколько может потребоваться дней в среднем, чтобы добиться результата похудения.
+        //Результаты всех подсчетов выведите в консоль.
 
         //Задача 8
-        int mashaSalaryOld = 67760;
-        int denisSalaryOld = 83690;
-        int chrisSalaryOld = 76230;
-        int mashaSalaryNew = (int) (mashaSalaryOld * 1.1f);
-        int denisSalaryNew = (int) (denisSalaryOld * 1.1f);
-        int chrisSalaryNew = (int) (chrisSalaryOld * 1.1f);
-        int mashaAnnualOld = mashaSalaryOld * 12;
-        int denisAnnualOld = denisSalaryOld * 12;
-        int chrisAnnualOld = chrisSalaryOld * 12;
-        int mashaAnnualNew = mashaSalaryNew * 12;
-        int denisAnnualNew = denisSalaryNew * 12;
-        int chrisAnnualNew = chrisSalaryNew * 12;
-        int mashaAnnualDelta = mashaAnnualNew - mashaAnnualOld;
-        int denisAnnualDelta = denisAnnualNew - denisAnnualOld;
-        int chrisAnnualDelta = chrisAnnualNew - chrisAnnualOld;
-        System.out.println("Маша теперь получает " + mashaSalaryNew + " рублей в месяц, годовой доход вырос на "
-                + mashaAnnualDelta + " рублей");
-        System.out.println("Денис теперь получает " + denisSalaryNew + " рублей в месяц, годовой доход вырос на "
-                + denisAnnualDelta + " рублей");
-        System.out.println("Кристина теперь получает " + chrisSalaryNew + " рублей в месяц, годовой доход вырос на "
-                + chrisAnnualDelta + " рублей");
+        //Представим, что мы работаем в большой компании, штат которой состоит из нескольких сотен сотрудников. В компании есть правило: чем дольше сотрудник работает в компании, тем ценнее он для бизнеса. Поэтому сотрудники, которые работают в компании дольше 3 лет, получают повышение зарплаты раз в год. Каждый год повышение составляет 10% от текущей зарплаты.
+        //К вам пришел руководитель с задачей автоматизировать повышение зарплаты, а также провести расчет для следующих сотрудников:
+        //Маша получает 67 760 рублей в месяц.
+        //Денис получает 83 690 рублей в месяц.
+        //Кристина получает 76 230 рублей в месяц.
+        //Каждому нужно увеличить зарплату на 10% от текущей месячной. Дополнительно руководитель попросил посчитать разницу между годовым доходом с нынешней зарплатой и после повышения.
+        //Посчитайте, сколько будет получать каждый из сотрудников, а также разницу между годовым доходом до и после повышения.
+        //Выведите в консоль информацию по каждому сотруднику. Например: «Маша теперь получает ... рублей. Годовой доход вырос на ... рублей».
+        }
     }
 }
